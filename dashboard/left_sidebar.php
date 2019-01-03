7f3c60804f66903503856a526961065b02da39f5
firdausillah <firdausillah123@gmail.com>
2019-01-03 10:03:58 +0700

    error

--- dashboard/left_sidebar.php
+++ dashboard/left_sidebar.php
@@ -40,12 +40,15 @@
 														foreach ($query2 as $key2 => $value2) {
 													?>
 													<li>
-														<a class="<?php echo (@$_GET['kelas']==$value2['kd_kelas'])?'active':'' ?>" href="../siswa/data_siswa.php?kelas=<?php echo $value2['kd_kelas'] ?>"><?php echo $value2['kelas']." ".$value2['golongan'] ?></a>
+														<a class="<?php echo (@$_GET['kelas']==$value2['kd_kelas'])?'active':'' ?>" href="../siswa/filter_data_siswa.php?kelas=<?php echo $value2['kd_kelas'] ?>"><?php echo $value2['kelas']." ".$value2['golongan'] ?></a>
 													</li>
 												<?php } ?>
 												</ul>
 											</li>
 										<?php } ?>
+										<li>
+											<a  href="../siswa/data_siswa.php" class="<?php echo ($nav == "guru" ? 'active' : '') ?>">Semua Data Siswa</a>
+										</li>
 								</ul>
 							</div>
 						</li>
@@ -110,12 +113,15 @@
 														foreach ($query2 as $key2 => $value2) {
 													?>
 													<li>
-														<a class="<?php echo (@$_GET['kelas']==$value2['kd_kelas'])?'active':'' ?>" href="../siswa/data_siswa.php?kelas=<?php echo $value2['kd_kelas'] ?>"><?php echo $value2['kelas']." ".$value2['golongan'] ?></a>
+														<a class="<?php echo (@$_GET['kelas']==$value2['kd_kelas'])?'active':'' ?>" href="../siswa/filer_data_siswa.php?kelas=<?php echo $value2['kd_kelas'] ?>"><?php echo $value2['kelas']." ".$value2['golongan'] ?></a>
 													</li>
 												<?php } ?>
 												</ul>
 											</li>
 										<?php } ?>
+										<li>
+											<a  href="../siswa/data_siswa.php" class="<?php echo ($nav == "siswa" ? 'active' : '') ?>">Data Siswa</a>
+										</li>
 								</ul>
 							</div>
 						</li>
--- mapel/delete_mapel.php
+++ mapel/delete_mapel.php
@@ -1,16 +1,12 @@
 <?php
     include '../koneksi.php';
-
+// print_r($_GET); exit();
     $kd_mapel = $_GET['kd_mapel'];
-    //$query = "DELETE FROM 'tbl_user' WHERE 'id'=$id";
-    //echo $query;
-   $query = mysqli_query($con, "DELETE FROM mapel WHERE kd_mapel=$kd_mapel");
-    if (mysqli_query($con, $query)) {
-        echo "Record deleted successfully";
-    } else {
-        echo "Error deleting record: " . mysqli_error($con);
-    }
-
-   header ("location:data_mapel.php");
+         if (mysqli_query($con, "DELETE FROM mapel WHERE kd_mapel='$kd_mapel'")) {
+            echo "Record deleted successfully";
+           header ("location:data_mapel.php");
+        } else {
+            echo "Error deleting record: " . mysqli_error($con);
+        }
 
 ?>
